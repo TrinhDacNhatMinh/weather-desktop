e2e15d00de95222fa1cf06d133e3f6de3f980e12
Trinh Dac Nhat Minh <trinhdnhatminh@gmail.com>
2025-12-20 23:21:46 +0700

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.nhom.weatherdesktop {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.nhom.weatherdesktop to javafx.fxml;
+    exports com.nhom.weatherdesktop;
+}
