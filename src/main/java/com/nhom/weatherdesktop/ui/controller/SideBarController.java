5abc6061b31041179db91263e33f2a685926fc1a
Trinh Dac Nhat Minh <trinhdnhatminh@gmail.com>
2025-12-22 17:18:37 +0700

    feat(ui): add sidebar layout to main screen

---
+++ src/main/java/com/nhom/weatherdesktop/ui/controller/SideBarController.java
@@ -0,0 +1,46 @@
+package com.nhom.weatherdesktop.ui.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+
+public class SideBarController {
+
+    @FXML
+    private Button myStationsButton;
+
+    @FXML
+    private Button publicStationsButton;
+
+    @FXML
+    private Button alertsButton;
+
+    @FXML
+    private Button settingsButton;
+
+    @FXML
+    public void initialize() {
+
+    }
+
+    @FXML
+    private void handleMyStations() {
+
+    }
+
+    @FXML
+    private void handlePublicStations() {
+
+    }
+
+    @FXML
+    private void handleAlerts() {
+
+    }
+
+    @FXML
+    private void handleSettings() {
+
+    }
+
+}
